89caceed27ff54b5600b552acebe956b3b884d1e
chlee1252 <chlee1252@gmail.com>
2019-12-03 02:06:33 -0800

    Initial dart file

---
+++ lib/widget/search.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/widgets.dart';
+
+class SearchPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new SearchPageState();
+  }
+}
+
+class SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Text("Search Search Search");
+  }
+}
