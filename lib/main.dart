e1f8a932e19e34490966a2a5c600db4ee88df7df
chlee1252 <chlee1252@gmail.com>
2019-12-03 16:59:48 -0800

    1st draft for infinite scrolling

--- lib/main.dart
+++ lib/main.dart
@@ -18,23 +18,50 @@ class MyApp extends StatefulWidget {
 
 class MyAppState extends State<MyApp> {
   var _isLoading = true;
+  var prevKeyword = '';
+  var pageNumber = 1;
+  var totalPages;
   final productData = new List<Product>();
   final url =
       "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&";
   final appIDURL = "SECURITY-APPNAME=" + appID + "&RESPONSE-DATA-FORMAT=JSON";
+  ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
-    _fetchData("nike");
+    _fetchData("nike", this.pageNumber.toString());
+
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+              _scrollController.position.maxScrollExtent &&
+          this.pageNumber < totalPages) {
+        this.pageNumber++;
+        _fetchData("nike oregon ducks backpack", this.pageNumber.toString());
+      }
+    });
   }
 
-  _fetchData(String keyword) async {
-    productData.clear();
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  _fetchData(String keyword, String pageNumber) async {
+    if (this.prevKeyword != keyword) {
+      // print("inside the if statement");
+      productData.clear();
+      this.prevKeyword = keyword;
+      this.pageNumber = 1;
+      pageNumber = "1";
+    }
+    // productData.clear();
     final completeURL = url +
         appIDURL +
         "&keywords=$keyword" +
-        "&paginationInput.entriesPerPage=2";
+        "&paginationInput.entriesPerPage=5" +
+        "&paginationInput.pageNumber=$pageNumber";
     final response = await http.get(completeURL);
 
     if (response.statusCode != 200) {
@@ -43,7 +70,8 @@ class MyAppState extends State<MyApp> {
       final dataJson = json.decode(response.body);
       final items =
           dataJson['findItemsByKeywordsResponse'][0]['searchResult'][0]["item"];
-
+      final totalpage = dataJson['findItemsByKeywordsResponse'][0]
+          ['paginationOutput'][0]['totalPages'][0];
       items.forEach((i) {
         final item = new Product(
             title: i['title'][0],
@@ -55,6 +83,7 @@ class MyAppState extends State<MyApp> {
             condition: i['condition'][0]['conditionDisplayName'][0]);
         productData.add(item);
       });
+      this.totalPages = int.parse(totalpage);
       setState(() {
         _isLoading = false;
       });
@@ -74,7 +103,8 @@ class MyAppState extends State<MyApp> {
               setState(() {
                 _isLoading = true;
               });
-              _fetchData("nike oregon ducks backpack");
+              _fetchData(
+                  "nike oregon ducks backpack", this.pageNumber.toString());
             },
           )
         ],
@@ -83,6 +113,7 @@ class MyAppState extends State<MyApp> {
           child: _isLoading
               ? new CircularProgressIndicator()
               : new ListView.builder(
+                  controller: _scrollController,
                   itemCount:
                       this.productData != null ? this.productData.length : 0,
                   itemBuilder: (context, i) {
